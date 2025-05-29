7764dbb92374860deeb6865df76adc85f6ca84aa
JC <olleresjcb@gmail.com>
2025-05-29 12:01:50 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlIn = New System.Windows.Forms.Panel()
+        Me.btnZOut = New System.Windows.Forms.Button()
+        Me.btnZIn = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnPlot = New System.Windows.Forms.Button()
+        Me.grpInput = New System.Windows.Forms.GroupBox()
+        Me.lblAmp = New System.Windows.Forms.Label()
+        Me.txtAmp = New System.Windows.Forms.TextBox()
+        Me.pnlOut = New System.Windows.Forms.Panel()
+        Me.picOut = New System.Windows.Forms.PictureBox()
+        Me.pnlIn.SuspendLayout()
+        Me.grpInput.SuspendLayout()
+        Me.pnlOut.SuspendLayout()
+        CType(Me.picOut, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pnlIn
+        '
+        Me.pnlIn.Controls.Add(Me.btnZOut)
+        Me.pnlIn.Controls.Add(Me.btnZIn)
+        Me.pnlIn.Controls.Add(Me.btnReset)
+        Me.pnlIn.Controls.Add(Me.btnPlot)
+        Me.pnlIn.Controls.Add(Me.grpInput)
+        Me.pnlIn.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.pnlIn.Location = New System.Drawing.Point(0, 253)
+        Me.pnlIn.Name = "pnlIn"
+        Me.pnlIn.Size = New System.Drawing.Size(532, 100)
+        Me.pnlIn.TabIndex = 0
+        '
+        'btnZOut
+        '
+        Me.btnZOut.Location = New System.Drawing.Point(414, 48)
+        Me.btnZOut.Name = "btnZOut"
+        Me.btnZOut.Size = New System.Drawing.Size(75, 32)
+        Me.btnZOut.TabIndex = 4
+        Me.btnZOut.Text = "Z&oom -"
+        Me.btnZOut.UseVisualStyleBackColor = True
+        '
+        'btnZIn
+        '
+        Me.btnZIn.Location = New System.Drawing.Point(414, 10)
+        Me.btnZIn.Name = "btnZIn"
+        Me.btnZIn.Size = New System.Drawing.Size(75, 32)
+        Me.btnZIn.TabIndex = 3
+        Me.btnZIn.Text = "&Zoom +"
+        Me.btnZIn.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(305, 48)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 32)
+        Me.btnReset.TabIndex = 2
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnPlot
+        '
+        Me.btnPlot.Location = New System.Drawing.Point(305, 10)
+        Me.btnPlot.Name = "btnPlot"
+        Me.btnPlot.Size = New System.Drawing.Size(75, 32)
+        Me.btnPlot.TabIndex = 1
+        Me.btnPlot.Text = "&Plot"
+        Me.btnPlot.UseVisualStyleBackColor = True
+        '
+        'grpInput
+        '
+        Me.grpInput.Controls.Add(Me.lblAmp)
+        Me.grpInput.Controls.Add(Me.txtAmp)
+        Me.grpInput.Location = New System.Drawing.Point(34, 10)
+        Me.grpInput.Name = "grpInput"
+        Me.grpInput.Size = New System.Drawing.Size(232, 74)
+        Me.grpInput.TabIndex = 0
+        Me.grpInput.TabStop = False
+        Me.grpInput.Text = "Input:"
+        '
+        'lblAmp
+        '
+        Me.lblAmp.AutoSize = True
+        Me.lblAmp.Location = New System.Drawing.Point(6, 37)
+        Me.lblAmp.Name = "lblAmp"
+        Me.lblAmp.Size = New System.Drawing.Size(85, 16)
+        Me.lblAmp.TabIndex = 1
+        Me.lblAmp.Text = "Amplitude, A:"
+        '
+        'txtAmp
+        '
+        Me.txtAmp.Location = New System.Drawing.Point(97, 34)
+        Me.txtAmp.Name = "txtAmp"
+        Me.txtAmp.Size = New System.Drawing.Size(129, 22)
+        Me.txtAmp.TabIndex = 0
+        '
+        'pnlOut
+        '
+        Me.pnlOut.Controls.Add(Me.picOut)
+        Me.pnlOut.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlOut.Location = New System.Drawing.Point(0, 0)
+        Me.pnlOut.Name = "pnlOut"
+        Me.pnlOut.Size = New System.Drawing.Size(532, 253)
+        Me.pnlOut.TabIndex = 1
+        '
+        'picOut
+        '
+        Me.picOut.BackColor = System.Drawing.SystemColors.Desktop
+        Me.picOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picOut.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.picOut.Location = New System.Drawing.Point(0, 0)
+        Me.picOut.Name = "picOut"
+        Me.picOut.Size = New System.Drawing.Size(532, 253)
+        Me.picOut.TabIndex = 0
+        Me.picOut.TabStop = False
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnPlot
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(532, 353)
+        Me.Controls.Add(Me.pnlOut)
+        Me.Controls.Add(Me.pnlIn)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Sine and Cosine Plotter"
+        Me.pnlIn.ResumeLayout(False)
+        Me.grpInput.ResumeLayout(False)
+        Me.grpInput.PerformLayout()
+        Me.pnlOut.ResumeLayout(False)
+        CType(Me.picOut, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlIn As Panel
+    Friend WithEvents btnZOut As Button
+    Friend WithEvents btnZIn As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnPlot As Button
+    Friend WithEvents grpInput As GroupBox
+    Friend WithEvents lblAmp As Label
+    Friend WithEvents txtAmp As TextBox
+    Friend WithEvents pnlOut As Panel
+    Friend WithEvents picOut As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,154 @@
+﻿Public Class Form1
+    'To be accessible to all
+    Dim intXOrigin, intYOrigin As Integer
+    Dim sngA As Single = 1
+    Dim sngZoom As Single = 0.5
+    Dim blnPlot As Boolean = False
+    Private Sub DrawAxes(ByVal g As Graphics)
+        'to put in the center
+        intXOrigin = picOut.Width \ 2
+        intYOrigin = picOut.Height \ 2
+        'initializing font
+        Dim myfont As New Font("Arial", 8, FontStyle.Regular)
+        g.Clear(Color.Black)
+        g.DrawLine(Pens.Gray, 0, intYOrigin, picOut.Width, intYOrigin)
+        g.DrawLine(Pens.Gray, intXOrigin, 0, intXOrigin, picOut.Height)
+        g.DrawString("(0, 0)", myfont, Brushes.Gray, intXOrigin - 15, intYOrigin + 5)
+        'to determine if the axes have labels or not
+        Select Case blnPlot
+            Case True
+                'for the labels to adjust
+                Dim radiansPerPixel As Single = 720 / picOut.Width * sngZoom
+                Dim tickstepX As Single = 15
+                Dim labelstepX As Single = 45
+                Dim scaleY As Single = (picOut.Height / 2) / (sngA * 2)
+                Dim yStep As Single = sngA
+                Dim xPixel, ypixel As Integer
+                Dim theta, yval As Single
+                'x axis
+                For theta = 15 To 720 Step tickstepX
+                    xPixel = intXOrigin + CInt(theta / radiansPerPixel)
+                    g.DrawLine(Pens.Gray, xPixel, intYOrigin - 3, xPixel, intYOrigin + 3)
+
+                    If theta Mod labelstepX = 0 Then
+                        g.DrawString(Math.Round(theta, 0).ToString(), myfont, Brushes.Gray, xPixel - 10, intYOrigin + 5)
+                    End If
+                Next
+
+                For theta = 15 To 720 Step tickstepX
+                    xPixel = intXOrigin - CInt(theta / radiansPerPixel)
+                    g.DrawLine(Pens.Gray, xPixel, intYOrigin - 3, xPixel, intYOrigin + 3)
+
+                    If theta Mod labelstepX = 0 Then
+                        g.DrawString("-" & Math.Round(theta, 0).ToString(), myfont, Brushes.Gray, xPixel - 15, intYOrigin + 5)
+                    End If
+                Next
+                'yaxis
+                For yval = yStep To sngA * 2 Step yStep
+                    ypixel = intYOrigin - CInt(yval * scaleY)
+                    g.DrawLine(Pens.Gray, intXOrigin - 3, ypixel, intXOrigin + 3, ypixel)
+                    g.DrawString(yval.ToString(), myfont, Brushes.Gray, intXOrigin + 5, ypixel - 8)
+                Next
+
+                For yval = yStep To sngA * 2 Step yStep
+                    ypixel = intYOrigin + CInt(yval * scaleY)
+                    g.DrawLine(Pens.Gray, intXOrigin - 3, ypixel, intXOrigin + 3, ypixel)
+                    g.DrawString("-" & yval.ToString(), myfont, Brushes.Gray, intXOrigin + 5, ypixel - 8)
+                Next
+                'change to the equation with the correct amplitude
+                g.DrawString("graph of the function", myfont, Brushes.Green, 0, 0)
+                g.DrawString("y = " & sngA & " sinx", myfont, Brushes.Coral, 0, 15)
+                g.DrawString("y = " & sngA & " cosx", myfont, Brushes.DodgerBlue, 0, 25)
+            Case False
+                'when no amplitude
+                g.DrawString("This program plots the general function" & vbNewLine & "y = A sinx and y = A cosx" & vbNewLine & "Please input the appropriate value of A" _
+                         & vbNewLine & "to the respective textbox", myfont, Brushes.Green, 0, 0)
+        End Select
+    End Sub
+    Private Sub picOut_Paint(sender As Object, e As PaintEventArgs) Handles picOut.Paint
+        Dim gpaint As Graphics = e.Graphics
+        'to have a proper output
+        'drawing the sin and cos line
+        Select Case blnPlot
+            Case True
+                Call DrawAxes(gpaint)
+
+                Dim penSin As New Pen(Color.Coral, 2)
+                Dim penCos As New Pen(Color.DodgerBlue, 2)
+                Dim xPixel As Integer
+                Dim xValue, ySin, yCos, prevX, prevYsin, prevYcos As Single
+                Dim scaleX As Single = sngZoom
+                Dim scaleY As Single = (picOut.Height / 2) / (sngA * 2)
+
+                For xPixel = 0 To picOut.Width
+                    xValue = (xPixel - intXOrigin) / (picOut.Width / (4 * Math.PI)) * scaleX
+                    ySin = intYOrigin - sngA * Math.Sin(xValue) * scaleY
+                    yCos = intYOrigin - sngA * Math.Cos(xValue) * scaleY
+
+                    If xPixel > 0 Then
+                        gpaint.DrawLine(penSin, prevX, prevYsin, xPixel, ySin)
+                        gpaint.DrawLine(penCos, prevX, prevYcos, xPixel, yCos)
+                    End If
+
+                    prevX = xPixel
+                    prevYsin = ySin
+                    prevYcos = yCos
+                Next
+            Case Else
+                gpaint.Clear(Color.Black)
+                Call DrawAxes(gpaint)
+        End Select
+    End Sub
+    Private Sub FormReset(sender As Object, e As EventArgs) Handles Me.Load, btnReset.Click
+        'reset when load or click
+        picOut.BackColor = Color.Black
+        blnPlot = False
+        sngZoom = 1
+        sngA = 1
+        txtAmp.Clear()
+        txtAmp.Focus()
+        txtAmp.ReadOnly = False
+        btnZIn.Enabled = False
+        btnZOut.Enabled = False
+        Me.Width = 550
+        Me.Height = 400
+        Me.CenterToScreen()
+        picOut.Refresh()
+    End Sub
+    Private Sub btnPlot_Click(sender As Object, e As EventArgs) Handles btnPlot.Click
+        Try
+            'to convert to single
+            sngA = CSng(txtAmp.Text)
+            'to filter out numbers less than or equal to 0
+            If sngA <= 0 Then
+                MessageBox.Show("The amplitude should be greater than 0.")
+                FormReset(Nothing, Nothing)
+                Exit Sub
+            End If
+
+            blnPlot = True
+            txtAmp.ReadOnly = True
+            btnZIn.Enabled = True
+            btnZOut.Enabled = True
+            picOut.Refresh()
+        Catch ex As Exception
+            'when other characters are inserted or other error are encountered
+            MessageBox.Show("Oops!!!", "Error")
+            FormReset(Nothing, Nothing)
+        End Try
+    End Sub
+    Private Sub btnZIn_Click(sender As Object, e As EventArgs) Handles btnZIn.Click
+        'zoom in by a factor of 2
+        sngZoom = sngZoom / 2
+        picOut.Refresh()
+    End Sub
+    Private Sub btnZOut_Click(sender As Object, e As EventArgs) Handles btnZOut.Click
+        'zoom out by a factor of 2
+        sngZoom = sngZoom * 2
+        picOut.Refresh()
+    End Sub
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        'redraw itself
+        picOut.Refresh()
+    End Sub
+End Class
